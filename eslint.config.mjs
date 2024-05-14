f97a11c5d774ce5d8a5cf816f3c89f9cad726952
CofCat456 <a25690756@gmail.com>
2024-05-14 10:24:48 +0800

    test: regex

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,12 @@
 // eslint.config.mjs
 import antfu from '@antfu/eslint-config'
+import nuxt from './packages/web/.nuxt/eslint.config.mjs'
 
-export default antfu({
-  formatters: true,
-})
+export default nuxt(
+  antfu(
+    {
+      unocss: true,
+      formatters: true,
+    },
+  ),
+)
